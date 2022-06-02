7d67131a61c87c2154fd54533723600bce032bfc
notexplosive <thenotexplosive@gmail.com>
2022-06-02 15:14:59 -0700

    Importing more code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import * as _ from "lodash";
 import { printMe } from "./print";
+import { cube } from "./math";
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement("pre");
 
-  element.innerHTML = _.join(["Hello", "webpack!"], " ");
+  // element.innerHTML = _.join(["Hello", "webpack!"], " ");
+  element.innerHTML = ["Hello webpack!", "5 cubed is " + cube(5)].join("\n\n");
 
   printMe();
 
