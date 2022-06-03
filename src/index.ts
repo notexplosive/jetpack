4bda2685e6198bafa748ba536c1103e92b8efa06
notexplosive <thenotexplosive@gmail.com>
2022-06-02 20:20:54 -0700

    Remove lodash

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-import * as _ from "lodash";
 import * as PIXI from "pixi.js";
 
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
 
+document.body.style.margin = "0";
+document.body.tabIndex = 1;
+document.body.style.background = "rgb(0, 0, 50)";
+
 let sprite = PIXI.Sprite.from("assets/sample.png");
 
-// Create the application helper and add its render target to the page
-let app = new PIXI.Application({ width: 640, height: 360 });
+let app = new PIXI.Application({
+  width: 800,
+  height: 600,
+  backgroundColor: 0x000000,
+});
+
 app.stage.addChild(sprite);
 document.body.appendChild(app.view);
