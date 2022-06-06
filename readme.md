1f280f83b2a3c28eb6186d423c7bda808bca9a5b
notexplosive <thenotexplosive@gmail.com>
2022-06-06 08:05:21 -0700

    Add readme

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Limbo Game Engine Template
+
+This is the template for the wrapper to run the Limbo engine.
+
+## Why
+
+This template gives you the following:
+
+- This is a ready-to-use pixi.js + typescript + webpack setup.
+- It also (optionally) includes [Limbo](https://github.com/notexplosive/limbo), my pixi.js game engine, if you don't want this you can just delete the directory after you clone the template. (at time of writing Limbo is in _very_ early development, it probably isn't useful to you)
+- You can run `npm start` and get a hot-reloaded development environment
+- You can run `npm build` and get a production build ready for itch.io
+
+## How do I use it?
+
+To run this template you'll need the latest version of Node (last confirmed working versions are `npm 8.12.1` and `node 16.15.1`).
+
+- Select `Use This Template` above.
+- Walk through the flow of creating a repo.
+- Run `npm install`
+- Run `npm start` to get a dev environment
+- Start writing code in `src/mygame/main.ts`
--- src/mygame/main.ts
+++ src/mygame/main.ts
@@ -1,4 +1,30 @@
 import * as PIXI from 'pixi.js';
+import { Rectangle } from 'pixi.js';
 import { game } from "../index";
+import { Updater } from '../limbo/data/updater';
+import { PrimitiveRenderer } from '../limbo/render/primitive';
 
-// your code starts here
+
+// game().updaters.push(new Updater((dt: number) => { console.log(dt) }));
+
+
+const plantRoot = game().worldContainer.addChild(new PIXI.Container());
+plantRoot.x = 300;
+plantRoot.y = 200;
+
+var plantRenderer = new PrimitiveRenderer(plantRoot);
+plantRenderer.circle(true, new PIXI.Point(0, 0), 25, { color: 0x00ff00 })
+
+var primitiveRenderer = new PrimitiveRenderer(game().app.stage);
+
+var filled = false;
+
+primitiveRenderer.rectangle(filled, new Rectangle(100, 100, 50, 50), { width: 1, color: 0xFFAAFF });
+
+primitiveRenderer.rectangle(filled, new Rectangle(500, 500, 50, 50), { width: 1, color: 0xFFFFAA });
+
+var lineStyle = { color: 0xFF0000, width: 1, join: PIXI.LINE_JOIN.ROUND }
+
+primitiveRenderer.line(new PIXI.Point(300, 300), new PIXI.Point(500, 200), lineStyle)
+
+primitiveRenderer.circle(filled, new PIXI.Point(200, 200), 50, lineStyle);
